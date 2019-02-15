b5079ad9d37c3a9bf84b0d98e18dd50d1744e64f
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-02-15 19:28:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 function init() {
   let recipeFormTemplate = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
   console.log(recipeFormTemplate);
-  document.getElementByTagName("main")[0].innerHTML += recipeFormTemplate;
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
